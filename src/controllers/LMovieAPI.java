530beb3e75cc09d85dbcd8b44633be4291205f16
conorf50 <conorf50@gmail.com>
2016-11-28 10:03:30 +0000

    Added in some more stuff from lectures

--- src/controllers/LMovieAPI.java
+++ src/controllers/LMovieAPI.java
@@ -1,5 +1,63 @@
 package controllers;
 
-public class LMovieAPI {
+import java.util.HashMap;
+import java.util.Map;
 
+import models.Movie;
+import models.User;
+
+public class LMovieAPI
+{
+	public Map<Long, User> userIndex = new HashMap<>();
+	public Map<Long, Movie> movieIndex = new HashMap<>();
+
+	public LMovieAPI()
+	{}
+
+	public User addUser(String firstName, String lastName, String age, String gender, String occupation)
+	{
+		User user = new User (firstName, lastName, age, gender, occupation);
+		userIndex.put(user.id, user);
+		return user;
+	}
+	public Movie addMovie(String title, String year, String url)
+	{
+		Movie movie = new Movie (title, year, url);
+		movieIndex.put(movie.id, movie);
+		return movie;
+	}
+
+	public void	removeUser(int userID) {
+	}
+
+
+	public void addRating(int userID, int movieID, int rating) {
+	}
+
+	public int getMovie(int movieID) {
+		return 0;
+	}
+
+	public int getUserRatings(int userID) {
+		return 0;
+	}
+
+	public int getUserRecommendations(int userID) {
+		return 0;
+	}
+
+	public String getTopTenMovies() {
+		return null;
+	}
+
+	public void load() {
+	}
+
+	public void write() {
+	}
+
+	public void store() {
+		// TODO Auto-generated method stub
+
+	}
 }
